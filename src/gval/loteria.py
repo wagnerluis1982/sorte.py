5e781820d592d047b106b387d987ac311d7287ff
Wagner Macedo <wagnerluis1982@gmail.com>
2014-03-25 00:50:15 -0300

    Gerando quina para qualquer quantidade

--- src/gval/loteria.py
+++ src/gval/loteria.py
@@ -4,8 +4,10 @@ class Loteria:
     def __init__(self, nome):
         pass
 
-    def gerar_aposta(self):
+    def gerar_aposta(self, quant=5):
+        if not (5 <= quant <= 80):
+            return ()
         result = set()
-        while len(result) < 5:
+        while len(result) < quant:
             result.add(random.randint(1, 80))
         return tuple(result)
--- test/test_quina.py
+++ test/test_quina.py
@@ -12,11 +12,21 @@ class QuinaTest:
     def test_gerar_aposta(self):
         permitidos = set(range(1, 81))  # de 1 a 80
 
+        # gerar quina na quantidade padrão == 5
         aposta = quina.gerar_aposta()
         assert_is_instance(aposta, tuple)
         eq_(len(aposta), 5, "aposta não tem 5 números")
         eq_(len(set(aposta)), 5, "aposta não tem 5 números diferentes")
         ok_(permitidos.issuperset(aposta), "aposta não está entre 1 e 80")
 
+        # outra aposta para identificar se realmente é aleatório
         aposta2 = quina.gerar_aposta()
         assert_not_equal(aposta, aposta2, "segunda aposta igual à primeira")
+
+        # apostas com mais de 5 números
+        for n in range(5, 10):
+            aposta = quina.gerar_aposta(n)
+            assert_is_instance(aposta, tuple)
+            eq_(len(aposta), n, "aposta não tem %d números" % n)
+            eq_(len(set(aposta)), n, "aposta não tem %d números diferentes" % n)
+            ok_(permitidos.issuperset(aposta), "aposta não está entre 1 e 80")
