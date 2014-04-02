afd23615570bbd53d66f7bbbcff0990bc0bfeeee
Wagner Macedo <wagnerluis1982@gmail.com>
2014-04-01 23:28:02 -0300

    Script de geração parametrizado

--- src/gval/loteria.py
+++ src/gval/loteria.py
@@ -4,7 +4,9 @@ class Loteria:
     def __init__(self, nome):
         pass
 
-    def gerar_aposta(self, quant=5):
+    def gerar_aposta(self, quant=None):
+        if quant is None:
+            quant = 5
         if not (5 <= quant <= 80):
             return ()
         result = random.sample(xrange(1, 81), quant)
--- src/gval/script.py
+++ src/gval/script.py
@@ -1,4 +1,5 @@
 # encoding=utf8
+from __future__ import print_function
 
 import getopt
 import sys
@@ -6,44 +7,64 @@ import sys
 from .loteria import Loteria
 
 def usage():
-	print "Uso: %s LOTERIA" % sys.argv[0]
-	print("Gerador e Verificador de Apostas da Loteria\n\n"
-    "Informando somente a LOTERIA, é gerada uma aposta\n\n"
-	"Argumentos:\n"
-	"  -h --help         mostra esta ajuda e finaliza")
+	print("Uso: %s LOTERIA" % sys.argv[0])
+	print("""Gerador e Verificador de Apostas da Loteria\n
+Informando somente a LOTERIA será gerada uma aposta
+
+Argumentos:
+  -q --quantidade   Quantas apostas deverão ser geradas. Padrão: 1
+  -n --numeros      Quantos números cada aposta gerada terá. Se não informado
+                      o padrão depende da LOTERIA informada
+  -h --help         Mostra esta ajuda e finaliza""")
 
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:],
         # opções curtas
-        "h",
+        "hn:q:",
         # opções longas
-        ["help"])
+        ["help", "numeros=", "quantidade="])
     except getopt.GetoptError, err:
         opt = (len(err.opt) == 1 and '-' or '--') + err.opt
-        print "opção", opt, "desconhecida"
+        print("opção", opt, "desconhecida")
         usage()
         sys.exit(1)
 
+    numeros = None
+    quantidade = 1
     for option, arg in opts:
         if option in ("-h", "--help"):
             usage()
             sys.exit(0)
+        elif option in ("-n", "--numeros"):
+            numeros = int(arg)
+        elif option in ("-q", "--quantidade"):
+            quantidade = int(arg)
 
-    if len(args) != 1:
-        print "deve ser informado um nome de loteria"
+    if len(args) == 0:
         usage()
-        sys.exit(2)
+        sys.exit(0)
+    if len(args) > 1:
+        print("deve ser informado apenas uma loteria")
+        usage()
+        sys.exit(3)
 
     nome = args[0]
     if nome.lower() not in ("quina",):
-        print "loteria '%s' não suportada" % nome
+        print("loteria '%s' não suportada" % nome)
         usage()
         sys.exit(3)
 
     lote = Loteria(nome)
-    print "Geração da", nome.title()
-    print "- Aposta:", ' '.join("%02d" % n for n in lote.gerar_aposta())
+    print("Geração de Aposta da", nome.title())
+    if quantidade == 1:
+        print(' '.join("%02d" % n for n in lote.gerar_aposta(numeros)))
+    else:
+        qdigitos = len(str(quantidade))
+        for i in xrange(1, quantidade+1):
+            print('#', '0'*(qdigitos - len(str(i))), "%d = " % i,
+                    ' '.join("%02d" % n for n in lote.gerar_aposta(numeros)),
+                    sep='')
 
 
 if __name__ == "__main__":
