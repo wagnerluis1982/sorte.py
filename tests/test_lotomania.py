250fce0d0aa5d951e4744c95daca9d439eefd296
Wagner Macedo <wagnerluis1982@gmail.com>
2014-04-06 00:34:51 -0300

    Correção na especificação do teste da lotomania
    Onde tinha escrito 50 números, deveria ser 20. Havia modificado o
    projeto, mas esqueci do teste.

--- tests/test_lotomania.py
+++ tests/test_lotomania.py
@@ -12,11 +12,11 @@ class LotomaniaTest(basetest.BaseTestCase):
     def test_gerar_aposta(_):
         permitidos = set(range(1, 101))  # de 1 a 100
 
-        # gerar lotomania na quantidade padrão == 50
+        # gerar lotomania na quantidade padrão == 20
         aposta = lotomania.gerar_aposta()
         _.is_instance(aposta, tuple)
-        _.eq(len(aposta), 50, "aposta não tem 50 números")
-        _.eq(len(set(aposta)), 50, "aposta não tem 50 números diferentes")
+        _.eq(len(aposta), 20, "aposta não tem 20 números")
+        _.eq(len(set(aposta)), 20, "aposta não tem 20 números diferentes")
         _.ok(permitidos.issuperset(aposta), "aposta não está entre 1 e 100")
 
         # outra aposta para identificar se realmente é aleatório
