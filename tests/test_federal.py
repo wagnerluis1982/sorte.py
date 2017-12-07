2e45155e100d10395dd4fb12065ac101f47aa5fa
Wagner Macedo <wagnerluis1982@gmail.com>
2017-12-06 23:44:19 -0300

    Refatoração em FederalTest
    Fazendo herança a partir de LoteriaTestCase, como os outros testes, mas
    com algumas mudanças:
    - Sobrescrita (override) do teste de consulta
    - Remoção do teste de gerar números

--- tests/test_federal.py
+++ tests/test_federal.py
@@ -1,15 +1,11 @@
-import unittest
 import basetest
-from sortepy.loterica import Loteria
 
 
-class FederalTest(unittest.TestCase):
+class FederalTest(basetest.LoteriaTestCase):
     'Loteria Federal'
 
-    def setUp(self):
-        self.loto = Loteria("federal", cfg_path=basetest.cfg_fixture_path)
-
     # atributos do teste de consulta
+    nome = "federal"
     concurso = 5235
     premios = {
         37715: "700.000,00",
@@ -24,25 +20,12 @@ class FederalTest(unittest.TestCase):
     esperados = {'acertou': ([[37715]], [[24797]], []),
                  'ganhou': (['700.000,00'], ['24.700,00'], ['0,00'])}
 
+    # teste com as regras específicas da federal
     def test_consultar_resultado(self):
         result = self.loto.consultar(self.concurso)
         assert isinstance(result, dict)
         assert result['concurso'] == self.concurso
         assert result['premios'] == self.premios
 
-    def test_conferir_aposta(self):
-        resp = self.loto.conferir(self.concurso, self.apostas[:1])
-        assert isinstance(resp, list)
-        assert resp[0]['concurso'] == self.concurso
-        assert resp[0]['numeros'] == self.apostas[0]
-        assert resp[0]['acertou'] == self.esperados['acertou'][0]
-        assert resp[0]['ganhou'] == self.esperados['ganhou'][0]
-
-    def test_conferir_varias_apostas(self):
-        resp = self.loto.conferir(self.concurso, self.apostas)
-        assert isinstance(resp, list)
-        for i in range(len(self.apostas)):
-            assert resp[i]['concurso'] == self.concurso
-            assert resp[i]['numeros'] == self.apostas[i]
-            assert resp[i]['acertou'] == self.esperados['acertou'][i]
-            assert resp[i]['ganhou'] == self.esperados['ganhou'][i]
+    # testes da superclasse removidos
+    test_gerar_aposta = None
