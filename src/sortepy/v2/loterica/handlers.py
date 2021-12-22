ce961212937f1880f281b59dd55f9b867d2242e7
Wagner Macedo <wagnerluis1982@gmail.com>
2021-12-22 02:04:24 +0100

    Refactoring

--- src/sortepy/v2/loterica/handlers.py
+++ src/sortepy/v2/loterica/handlers.py
@@ -2,37 +2,24 @@ from __future__ import annotations
 
 from datetime import datetime
 
-from sortepy.v2.loterica._types import ResultadoT1Dict
+from sortepy.v2.loterica._types import LotericaResponseDict
 from sortepy.v2.spi import DrawResponseHandler
 from sortepy.v2.types import DrawDetail
 from sortepy.v2.types import DrawPrize
 from sortepy.v2.types import DrawResult
 
 
-class LotofacilResponseHandler(DrawResponseHandler[ResultadoT1Dict]):
-    def handle(self, data: ResultadoT1Dict) -> DrawResult:
-        hit_mapping = {
-            "15 acertos": "15",
-            "14 acertos": "14",
-            "13 acertos": "13",
-            "12 acertos": "12",
-            "11 acertos": "11",
-        }
-        return _handle_response_type1(data, hit_mapping)
+class LotofacilResponseHandler(DrawResponseHandler[LotericaResponseDict]):
+    def handle(self, data: LotericaResponseDict) -> DrawResult:
+        return _handle_response_type1(data)
 
 
-class QuinaResponseHandler(DrawResponseHandler[ResultadoT1Dict]):
-    def handle(self, data: ResultadoT1Dict) -> DrawResult:
-        hit_mapping = {
-            "5 acertos": "5",
-            "4 acertos": "4",
-            "3 acertos": "3",
-            "2 acertos": "2",
-        }
-        return _handle_response_type1(data, hit_mapping)
+class QuinaResponseHandler(DrawResponseHandler[LotericaResponseDict]):
+    def handle(self, data: LotericaResponseDict) -> DrawResult:
+        return _handle_response_type1(data)
 
 
-def _handle_response_type1(data: ResultadoT1Dict, hit_mapping: dict) -> DrawResult:
+def _handle_response_type1(data: LotericaResponseDict) -> DrawResult:
     return DrawResult(
         draw_date=datetime.strptime(data["dataApuracao"], "%d/%m/%Y"),
         draw_number=str(data["numero"]),
@@ -41,16 +28,16 @@ def _handle_response_type1(data: ResultadoT1Dict, hit_mapping: dict) -> DrawResu
                 ball_numbers=[int(n) for n in data["listaDezenas"]],
                 jackpot=data["valorAcumuladoProximoConcurso"],
                 currency="BRL",
-                prize_breakdown=_get_prizes_type1(data, hit_mapping),
+                prize_breakdown=_get_prizes_type1(data),
             )
         ],
     )
 
 
-def _get_prizes_type1(data: ResultadoT1Dict, hit_mapping: dict) -> dict[str, DrawPrize]:
+def _get_prizes_type1(data: LotericaResponseDict) -> dict[str, DrawPrize]:
     prizes = {}
     for p in data["listaRateioPremio"]:
-        key = hit_mapping[p["descricaoFaixa"]]
+        key = p["descricaoFaixa"].split()[0]
         value = DrawPrize(winners=p["numeroDeGanhadores"], prize=p["valorPremio"])
         prizes[key] = value
 
--- tests/v2/loterica/test_handlers.py
+++ tests/v2/loterica/test_handlers.py
@@ -5,57 +5,47 @@ from pathlib import Path
 
 from sortepy.v2.loterica.handlers import LotofacilResponseHandler
 from sortepy.v2.loterica.handlers import QuinaResponseHandler
-from sortepy.v2.types import DrawDetail
 from sortepy.v2.types import DrawPrize
-from sortepy.v2.types import DrawResult
 
 
 def test_lotofacil_handler(fixtures_dir: Path):
     handler = LotofacilResponseHandler()
     data = json.load(open(fixtures_dir / "response_lotofacil_2402.json"))
 
-    actual_result = handler.handle(data)
+    result = handler.handle(data)
+    assert result.draw_date == datetime(year=2021, month=12, day=20)
+    assert result.draw_number == "2402"
+    assert len(result.draw_details) == 1
 
-    assert actual_result == DrawResult(
-        draw_date=datetime(year=2021, month=12, day=20),
-        draw_number="2402",
-        draw_details=[
-            DrawDetail(
-                ball_numbers=[1, 2, 3, 4, 5, 6, 9, 10, 11, 13, 14, 18, 20, 23, 25],
-                jackpot=0.00,
-                currency="BRL",
-                prize_breakdown={
-                    "15": DrawPrize(winners=3, prize=370_337.82),
-                    "14": DrawPrize(winners=321, prize=1036.73),
-                    "13": DrawPrize(winners=13_409, prize=25.00),
-                    "12": DrawPrize(winners=144_941, prize=10.00),
-                    "11": DrawPrize(winners=741_668, prize=5.00),
-                },
-            )
-        ],
-    )
+    draw = result.draw_details[0]
+    assert draw.ball_numbers == [1, 2, 3, 4, 5, 6, 9, 10, 11, 13, 14, 18, 20, 23, 25]
+    assert draw.jackpot == 0.00
+    assert draw.currency == "BRL"
+    assert draw.prize_breakdown == {
+        "15": DrawPrize(winners=3, prize=370_337.82),
+        "14": DrawPrize(winners=321, prize=1036.73),
+        "13": DrawPrize(winners=13_409, prize=25.00),
+        "12": DrawPrize(winners=144_941, prize=10.00),
+        "11": DrawPrize(winners=741_668, prize=5.00),
+    }
 
 
 def test_quina_handler(fixtures_dir: Path):
     handler = QuinaResponseHandler()
     data = json.load(open(fixtures_dir / "response_quina_5734.json"))
 
-    actual_result = handler.handle(data)
+    result = handler.handle(data)
+    assert result.draw_date == datetime(year=2021, month=12, day=18)
+    assert result.draw_number == "5734"
+    assert len(result.draw_details) == 1
 
-    assert actual_result == DrawResult(
-        draw_date=datetime(year=2021, month=12, day=18),
-        draw_number="5734",
-        draw_details=[
-            DrawDetail(
-                ball_numbers=[26, 27, 29, 74, 77],
-                jackpot=10_358_517.22,
-                currency="BRL",
-                prize_breakdown={
-                    "5": DrawPrize(winners=0, prize=0.00),
-                    "4": DrawPrize(winners=61, prize=8093.88),
-                    "3": DrawPrize(winners=6120, prize=76.83),
-                    "2": DrawPrize(winners=151744, prize=3.09),
-                },
-            )
-        ],
-    )
+    draw = result.draw_details[0]
+    assert draw.ball_numbers == [26, 27, 29, 74, 77]
+    assert draw.jackpot == 10_358_517.22
+    assert draw.currency == "BRL"
+    assert draw.prize_breakdown == {
+        "5": DrawPrize(winners=0, prize=0.00),
+        "4": DrawPrize(winners=61, prize=8093.88),
+        "3": DrawPrize(winners=6120, prize=76.83),
+        "2": DrawPrize(winners=151744, prize=3.09),
+    }
